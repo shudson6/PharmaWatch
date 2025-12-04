5328ce46ecbadc18fb3dba59538ad187afd05e73
Steven Hudson <56903686+shudson6@users.noreply.github.com>
2025-12-03 22:13:37 -0600

    derive agio and capr from Monitor; add helper funcs for repl sessions

--- agio.py
+++ agio.py
@@ -1,37 +1,28 @@
-from datetime import datetime
 import logging
 import os
 
 import pymupdf4llm
-import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
+
+from Monitor import Monitor
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 DEFAULT_DOWNLOAD_DIR = os.path.join(os.getcwd(), "downloads")
 
-class AgioMonitor:
+class AgioMonitor(Monitor):
     def __init__(self):
-        self.symbol = "AGIO"
-        self.news_url = "https://investor.agios.com/news-events/press-releases"
+        super().__init__(
+            symbol = "AGIO",
+            press_release_url = "https://investor.agios.com/news-events/press-releases",
+        )
 
     def fetch_news_articles(self):
-        logger.debug("Starting Chrome WebDriver")
-        options = Options()
-        options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
-        driver = webdriver.Chrome(
-            options=options,
-            service=ChromeService(ChromeDriverManager().install())
-        )
-        logger.debug("Chrome WebDriver started")
-        driver.get(self.news_url)
+        driver = self.start_web_driver()
+        driver.get(self.press_release_url)
         container = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, "article"))
         )
@@ -64,34 +55,3 @@ class AgioMonitor:
             except Exception as e:
                 logging.warning(f"{type(e)} occurred while loading article {a['title'][:32]}:\n{e}")
         return article_data
-
-    def download_file(self, url, dest_dir=DEFAULT_DOWNLOAD_DIR, 
-                    session=None, timeout=30):
-        session = session or requests.Session()
-        logger.info(f"Downloading file from {url}")
-        try:
-            response = session.get(url, stream=True, timeout=timeout)
-            response.raise_for_status()
-            filename = "download-{}-{}.pdf"
-            filename = filename.format(self.symbol,
-                                       datetime.now().strftime('%Y%m%d%H%M%S'))
-            os.makedirs(dest_dir, exist_ok=True)
-
-            # ensure unique filename
-            dest_path = os.path.join(dest_dir, filename)
-            base, ext = os.path.splitext(dest_path)
-            counter = 1
-            while os.path.exists(dest_path):
-                dest_path = f"{base}_{counter}{ext}"
-                counter += 1
-
-            logger.debug(f"Attempting to save {dest_path}")
-            with open(dest_path, 'wb') as fp:
-                for chunk in response.iter_content(chunk_size=8192):
-                    fp.write(chunk)
-            logger.info(f"Saved {url} -> {dest_path}")
-            return dest_path
-        
-        except Exception as e:
-            logger.warning(f"Failed to get {url}: {e}")
-            return None
