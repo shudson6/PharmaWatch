b378e2905740d8816650b397277628a2cfb1d389
Steven Hudson <56903686+shudson6@users.noreply.github.com>
2025-12-06 00:02:00 -0600

    put MonitoringService in an infinite loop

--- services/MonitoringService.py
+++ services/MonitoringService.py
@@ -14,25 +14,28 @@ def resolve_monitor_name(symbol: str):
 
 def start():
     logger.info("Starting Monitoring Service")
-    watchlist = db.get_watch_list()
-    logger.debug(f"Found {len(watchlist)} watches: {watchlist}")
-    for symbol in watchlist:
-        logger.debug(f"Monitor for {symbol}: {resolve_monitor_name(symbol)}")
-        module_name, class_name = resolve_monitor_name(symbol)
-        monitor: MonitorBase
-        monitor = getattr(importlib.import_module(module_name), class_name)()
-        new_articles = monitor.fetch_news_articles()
-        logger.info(f"Found {len(new_articles)} new articles for {symbol}")
-        for a in new_articles:
-            try:
-                a['pr_id'] = db.save_new_article(
-                    symbol, a['date'], a['title'], a['content-type'],
-                    a['content'], a['document_url'], a['retrieved_ts']
-                )
-            except Exception as e:
-                logger.warning(f"{type(e).__name__} occurred while saving"
-                               f"article {a['title']}. Article not saved")
-        for a in new_articles:
-            if "pr_id" in a:
-                SummarizationService.queue_article(a)
-    logger.info("MonitoringService finished.")
+    while True:
+        watchlist = db.get_watch_list()
+        logger.debug(f"Found {len(watchlist)} watches: {watchlist}")
+        for symbol in watchlist:
+            logger.debug(f"Monitor for {symbol}: {resolve_monitor_name(symbol)}")
+            module_name, class_name = resolve_monitor_name(symbol)
+            monitor: MonitorBase
+            monitor = getattr(importlib.import_module(module_name), class_name)()
+            new_articles = monitor.fetch_news_articles()
+            logger.info(f"Found {len(new_articles)} new articles for {symbol}")
+            for a in new_articles:
+                try:
+                    a['pr_id'] = db.save_new_article(
+                        symbol, a['date'], a['title'], a['content-type'],
+                        a['content'], a['document_url'], a['retrieved_ts']
+                    )
+                except Exception as e:
+                    logger.warning(f"{type(e).__name__} occurred while saving"
+                                f"article {a['title']}. Article not saved")
+            for a in new_articles:
+                if "pr_id" in a:
+                    SummarizationService.queue_article(a)
+        logger.info("MonitoringService finished. Restarting in 5 minutes.")
+        import time
+        time.sleep(300)
