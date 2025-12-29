120a32f89471a95e704d6570975b2debb6f5ff2c
Steven Hudson <56903686+shudson6@users.noreply.github.com>
2025-12-29 11:07:11 -0600

    make app.py importable

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from enum import StrEnum
 import logging
 import os
 
@@ -7,30 +8,39 @@ from dotenv import load_dotenv
 
 from services import MonitoringService, NewsAnalysisService
 
-file_handler = logging.FileHandler(
-    os.path.join(os.getcwd(), "logs",
-                 "pharmawatch-" + datetime.now().strftime("%Y%m%d-%H%M") + ".log"),
-    encoding="utf-8",
-    delay=True,
-)
-stream_handler = logging.StreamHandler()
-logging.basicConfig(
-    format="%(asctime)s:%(levelname)s:%(name)s:%(lineno)d: %(message)s",
-    handlers=[file_handler, stream_handler],
-)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-os.makedirs(os.path.join(os.getcwd(), "logs"), exist_ok=True)
-
-logger.info("Starting PharmaWatch")
 load_dotenv()
 
-logger.info("Firing up ThreadPoolExecutor and scheduling jobs")
-executor = ThreadPoolExecutor(max_workers=3)
-executor.submit(NewsAnalysisService.start)
-executor.submit(NewsAnalysisService.queue_unsummarized_articles)
-executor.submit(MonitoringService.start)
+def setup_logging():
+    file_handler = logging.FileHandler(
+        os.path.join(os.getcwd(), "logs",
+                    "pharmawatch-" + datetime.now().strftime("%Y%m%d-%H%M") + ".log"),
+        encoding="utf-8",
+        delay=True,
+    )
+    stream_handler = logging.StreamHandler()
 
-# wait til jobs are done, which will never happen since they're infinite loops :)
-executor.shutdown(wait=True)
+    logging.basicConfig(
+        format="%(asctime)s:%(levelname)s:%(name)s:%(lineno)d: %(message)s",
+        handlers=[file_handler, stream_handler],
+    )
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    os.makedirs(os.path.join(os.getcwd(), "logs"), exist_ok=True)
+    return logger
+
+def start():
+    logger = setup_logging()
+    logger.info("Starting PharmaWatch")
+
+    logger.info("Firing up ThreadPoolExecutor and scheduling jobs")
+    executor = ThreadPoolExecutor(max_workers=3)
+    executor.submit(NewsAnalysisService.start)
+    executor.submit(NewsAnalysisService.queue_unsummarized_articles)
+    executor.submit(MonitoringService.start)
+
+    # wait til jobs are done, which will never happen since they're infinite loops :)
+    executor.shutdown(wait=True)
+
+if __name__ == "__main__":
+    start()
