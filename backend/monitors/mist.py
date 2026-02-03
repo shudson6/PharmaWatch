d7624e022da135c83edcfc7cc653c3ef54c5d759
Steven Hudson <56903686+shudson6@users.noreply.github.com>
2026-02-03 21:35:49 +0000

    remove old monitor classes

--- backend/monitors/mist.py
+++
@@ -1,8 +0,0 @@
-from monitors.aldx import AldxMonitor
-
-class MistMonitor(AldxMonitor):
-    def __init__(self,
-                 symbol = "MIST",
-                 press_release_url = "https://investors.milestonepharma.com/press-releases",
-                 ):
-        super().__init__(symbol, press_release_url)
